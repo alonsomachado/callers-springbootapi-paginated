fb6ece5f5b90e8500492f8e2e52320785228bb84
Alonso Machado <alonsomachado2@gmail.com>
2021-04-19 23:22:57 +0100

    Spring Intilializr
    Spring Intilializr

---
+++ src/test/java/com/alonso/callers/CallersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alonso.callers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
