830c9064ef61f1ac92a554909ece7adfbc068a28
Alonso Machado <alonsomachado2@gmail.com>
2021-04-20 00:04:06 +0100

    Call Controller

--- src/main/java/com/alonso/callers/controllers/CallController.java
+++ src/main/java/com/alonso/callers/controllers/CallController.java
@@ -3,14 +3,15 @@ package com.alonso.callers.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class CallController {
 
 	public ResponseEntity<Map<String, Object>> getAll(
-			@RequestParam(required = false) String title,
 			@RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "5") int size
 	) {
--- src/main/java/com/alonso/callers/controllers/ICallController.java
+++ src/main/java/com/alonso/callers/controllers/ICallController.java
@@ -1,10 +1,9 @@
 package com.alonso.callers.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -16,4 +15,13 @@ public interface ICallController {
             @RequestParam(required = false) String title,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size);
+
+    @PostMapping //Decidir no POST se usar Request Body ou Request Param
+    public ResponseEntity<Map<String, Object>> addCall();
+
+    //TODO Statistics - Criar um controller separado e Model para a estatisticas com o AOP
+
+    @DeleteMapping
+    public ResponseEntity<Map<String, Object>> deleteCall(@RequestParam @NonNull Long id);
+
 }
